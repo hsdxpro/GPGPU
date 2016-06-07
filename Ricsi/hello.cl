34b93c0feca879ca68258cdf07278b2c26cd2069
hsdxpro <hsdxpro@gmail.com>
2016-06-07 10:35:22 +0200

    measure

--- Ricsi/hello.cl
+++ Ricsi/hello.cl
@@ -1,5 +1,5 @@
-constant int resX = 640;
-constant int resY = 480;
+constant int resX = 64 * 20;
+constant int resY = 64 * 10;
 
 class Raytracer
 {
@@ -11,12 +11,12 @@ kernel void hello(global uchar* out)
 {
 	size_t x = get_global_id(0) * 3;
 	size_t y = get_global_id(1) * 3;
-
+	
 	Raytracer rt;
 	uchar r = rt.GetColor().x;
 	uchar g = rt.GetColor().y;
 	uchar b = rt.GetColor().z;
-
+	
 	out[x + resX * y + 0] = r;
 	out[x + resX * y + 1] = g;
 	out[x + resX * y + 2] = b;
--- Ricsi/ricsi.h
+++ Ricsi/ricsi.h
@@ -18,15 +18,93 @@
 #include <cassert>
 #include <string>
 #include <windows.h>
+#include <chrono>
+using namespace std::chrono;
 
 const std::string hw("Hello World\n");
 
+const char *getErrorString(cl_int error)
+{
+switch(error){
+    // run-time and JIT compiler errors
+    case 0: return "CL_SUCCESS";
+    case -1: return "CL_DEVICE_NOT_FOUND";
+    case -2: return "CL_DEVICE_NOT_AVAILABLE";
+    case -3: return "CL_COMPILER_NOT_AVAILABLE";
+    case -4: return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
+    case -5: return "CL_OUT_OF_RESOURCES";
+    case -6: return "CL_OUT_OF_HOST_MEMORY";
+    case -7: return "CL_PROFILING_INFO_NOT_AVAILABLE";
+    case -8: return "CL_MEM_COPY_OVERLAP";
+    case -9: return "CL_IMAGE_FORMAT_MISMATCH";
+    case -10: return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
+    case -11: return "CL_BUILD_PROGRAM_FAILURE";
+    case -12: return "CL_MAP_FAILURE";
+    case -13: return "CL_MISALIGNED_SUB_BUFFER_OFFSET";
+    case -14: return "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST";
+    case -15: return "CL_COMPILE_PROGRAM_FAILURE";
+    case -16: return "CL_LINKER_NOT_AVAILABLE";
+    case -17: return "CL_LINK_PROGRAM_FAILURE";
+    case -18: return "CL_DEVICE_PARTITION_FAILED";
+    case -19: return "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";
+
+    // compile-time errors
+    case -30: return "CL_INVALID_VALUE";
+    case -31: return "CL_INVALID_DEVICE_TYPE";
+    case -32: return "CL_INVALID_PLATFORM";
+    case -33: return "CL_INVALID_DEVICE";
+    case -34: return "CL_INVALID_CONTEXT";
+    case -35: return "CL_INVALID_QUEUE_PROPERTIES";
+    case -36: return "CL_INVALID_COMMAND_QUEUE";
+    case -37: return "CL_INVALID_HOST_PTR";
+    case -38: return "CL_INVALID_MEM_OBJECT";
+    case -39: return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
+    case -40: return "CL_INVALID_IMAGE_SIZE";
+    case -41: return "CL_INVALID_SAMPLER";
+    case -42: return "CL_INVALID_BINARY";
+    case -43: return "CL_INVALID_BUILD_OPTIONS";
+    case -44: return "CL_INVALID_PROGRAM";
+    case -45: return "CL_INVALID_PROGRAM_EXECUTABLE";
+    case -46: return "CL_INVALID_KERNEL_NAME";
+    case -47: return "CL_INVALID_KERNEL_DEFINITION";
+    case -48: return "CL_INVALID_KERNEL";
+    case -49: return "CL_INVALID_ARG_INDEX";
+    case -50: return "CL_INVALID_ARG_VALUE";
+    case -51: return "CL_INVALID_ARG_SIZE";
+    case -52: return "CL_INVALID_KERNEL_ARGS";
+    case -53: return "CL_INVALID_WORK_DIMENSION";
+    case -54: return "CL_INVALID_WORK_GROUP_SIZE";
+    case -55: return "CL_INVALID_WORK_ITEM_SIZE";
+    case -56: return "CL_INVALID_GLOBAL_OFFSET";
+    case -57: return "CL_INVALID_EVENT_WAIT_LIST";
+    case -58: return "CL_INVALID_EVENT";
+    case -59: return "CL_INVALID_OPERATION";
+    case -60: return "CL_INVALID_GL_OBJECT";
+    case -61: return "CL_INVALID_BUFFER_SIZE";
+    case -62: return "CL_INVALID_MIP_LEVEL";
+    case -63: return "CL_INVALID_GLOBAL_WORK_SIZE";
+    case -64: return "CL_INVALID_PROPERTY";
+    case -65: return "CL_INVALID_IMAGE_DESCRIPTOR";
+    case -66: return "CL_INVALID_COMPILER_OPTIONS";
+    case -67: return "CL_INVALID_LINKER_OPTIONS";
+    case -68: return "CL_INVALID_DEVICE_PARTITION_COUNT";
+
+    // extension errors
+    case -1000: return "CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR";
+    case -1001: return "CL_PLATFORM_NOT_FOUND_KHR";
+    case -1002: return "CL_INVALID_D3D10_DEVICE_KHR";
+    case -1003: return "CL_INVALID_D3D10_RESOURCE_KHR";
+    case -1004: return "CL_D3D10_RESOURCE_ALREADY_ACQUIRED_KHR";
+    case -1005: return "CL_D3D10_RESOURCE_NOT_ACQUIRED_KHR";
+    default: return "Unknown OpenCL error";
+    }
+}
 
 void checkErr(cl_int err, const char * name)
 {
 	if (err != CL_SUCCESS)
 	{
-		std::cerr << "ERROR: " << name << " (" << err << ")" << std::endl;
+		std::cerr << "ERROR: " << name << " (" << getErrorString( err ) << ")" << std::endl;
 		assert(0);
 	}
 }
@@ -59,7 +137,7 @@ void ricsi_hello()
 
 	// Create context
 	cl_context_properties contextDesc[3] = { CL_CONTEXT_PLATFORM, (cl_context_properties)(selectedPlatform)(), 0 }; 
-	cl::Context context(CL_DEVICE_TYPE_CPU, contextDesc, NULL, NULL, &err);
+	cl::Context context(CL_DEVICE_TYPE_GPU, contextDesc, NULL, NULL, &err);
 	checkErr(err, "Context creation");
 
 	// Get devices
@@ -70,8 +148,8 @@ void ricsi_hello()
 	// Select device
 	cl::Device& selectedDevice = devices[0];
 
-	int resX = 640;
-	int resY = 480;
+	int resX = 64 * 20;
+	int resY = 64 * 10;
 	int bytesPerPixel = 3;
 
 	// Allocate memory
@@ -92,22 +170,6 @@ void ricsi_hello()
 	// Create kernel
 	cl::Kernel kernel(program, "hello", &err);
 	checkErr(err, "Kernel::Kernel()");
-	err = kernel.setArg(0, outCL);
-	checkErr(err, "Kernel::setArg()");
-
-	// Run kernel
-	cl::CommandQueue queue(context, selectedDevice, 0, &err);
-	checkErr(err, "CommandQueue::CommandQueue()");
-	cl::Event event;
-	err = queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(resX, resY), cl::NDRange(1, 1), NULL, &event);
-	checkErr(err, "ComamndQueue::enqueueNDRangeKernel()");
-
-	// Wait kernel
-	event.wait();
-
-	// Read back result from GPU memory to CPU
-	err = queue.enqueueReadBuffer(outCL, CL_TRUE, 0, resX * resY * bytesPerPixel, backbuffer);
-	checkErr(err, "ComamndQueue::enqueueReadBuffer()");
 
 	
 	// Init SDL
@@ -120,44 +182,65 @@ void ricsi_hello()
 	// Create window
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window *MainWindow = SDL_CreateWindow("GPGPU",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        resX, resY,
-        SDL_WINDOW_SHOWN
-        );
+    SDL_Window* mainWindow = SDL_CreateWindow("GPGPU", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, resX, resY, SDL_WINDOW_SHOWN );
+    SDL_Renderer *renderer = SDL_CreateRenderer(mainWindow, -1, 0);
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(MainWindow, -1, 0);
-
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
+    //SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
     SDL_Texture* screenTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STREAMING, resX, resY);
 
+	cl::CommandQueue queue(context, selectedDevice, 0, &err);
+	checkErr(err, "CommandQueue::CommandQueue()");
+	cl::Event eventt;
+
 	while (1) 
 	{
+		// Start measure
+		high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
 		SDL_Event event;
 		while (SDL_PollEvent(&event))
 		{
-
+		
 		}
 
-		{
-			// Initialize texture pixels to a red opaque RGBA value
-			unsigned char* bytes = nullptr;
-			int pitch = 0;
-			SDL_LockTexture(screenTexture, nullptr, reinterpret_cast<void**>(&bytes), &pitch);
-			memcpy(bytes, backbuffer, resX * resY * bytesPerPixel);
-			SDL_UnlockTexture(screenTexture);
+		// Set args
+		err = kernel.setArg(0, outCL);
+		checkErr(err, "Kernel::setArg()");
 
-			//SDL_Rect destination = { 0, 0, resX, resY };
-			SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
-			SDL_RenderPresent(renderer);
-		}
+		// Run kernel
+		err = queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(resX, resY), cl::NDRange(16,16), NULL, &eventt);
+		checkErr(err, "ComamndQueue::enqueueNDRangeKernel()");
+
+		// Wait kernel
+		eventt.wait();
+		
+		// Read back result from GPU memory to CPU
+		err = queue.enqueueReadBuffer(outCL, CL_TRUE, 0, resX * resY * bytesPerPixel, backbuffer);
+		checkErr(err, "ComamndQueue::enqueueReadBuffer()");
+		
+		// TODO VERY SLOW
+		// Initialize texture pixels to a red opaque RGBA value
+		unsigned char* bytes = nullptr;
+		int pitch = 0;
+		SDL_LockTexture(screenTexture, nullptr, reinterpret_cast<void**>(&bytes), &pitch);
+		memcpy(bytes, backbuffer, resX * resY * bytesPerPixel);
+		SDL_UnlockTexture(screenTexture);
+		
+		//SDL_Rect destination = { 0, 0, resX, resY };
+		SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+
+		// End measure
+		high_resolution_clock::time_point t2 = high_resolution_clock::now();
+		auto duration = duration_cast<microseconds>( t2 - t1 ).count();
+
+		std::string str = std::to_string(duration / 1000.0);
+		SDL_SetWindowTitle(mainWindow, str.c_str());
 	}
 
     //Clean up
     SDL_DestroyTexture(screenTexture);
-    SDL_DestroyWindow(MainWindow);
+    SDL_DestroyWindow(mainWindow);
     SDL_Quit();
 }
